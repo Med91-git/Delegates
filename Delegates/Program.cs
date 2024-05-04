ea3b6efe4cb2a0b9f1f8ac77b748c819c3f20fe0
Mederic <kottomouyema.m@gmail.com>
2024-05-04 13:24:01 +0200

    Principe Delegate ok - Probleme affichage de la somme en euro

---
+++ Delegates/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Delegates
+{
+    internal class Program
+    {
+        public delegate void ShowMoney(int valeur);
+
+        public static void DisplayDollarMoney(int valeur)
+        {
+            Console.WriteLine(valeur + " $");
+
+        }
+        public static void DisplayEuroMoney(int valeur)
+        {
+            Console.WriteLine(valeur + " €");
+
+        }
+
+        static void Main(string[] args)
+        {
+            int somme = 2;
+            ShowMoney affichageEnDollar = DisplayDollarMoney;  
+            Console.WriteLine("Affichage de la somme en dollar : ");
+            affichageEnDollar(somme);
+            
+            Console.WriteLine();
+            
+            Console.WriteLine("Affichage de la somme en euro :");
+            ShowMoney affichageEnEuro = DisplayEuroMoney;  
+            affichageEnEuro(somme);   
+
+
+        }
+    }
+}
